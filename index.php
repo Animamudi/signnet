af7d92047c4375000feeed3dc2511869bbdf7c0a
Animamudi <94235591+Animamudi@users.noreply.github.com>
2019-02-28 11:01:52 +0000

    x86_64, traps: Rework bad_iret

--- index.php
+++ index.php
@@ -204,3 +204,5 @@ $app->get('/balances', function() use ($app,&$mysqli) {
 //   array of balance information (mandatory)
 // Result (JSON body):
 //   status=OK|ERROR
+//   messages=array of error messages (only if status is ERROR)
+//   data=array of insert/update information (only if status is OK)
