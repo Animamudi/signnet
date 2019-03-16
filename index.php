861b2e98a97f38adfbe6107be9d5ec67c9274072
Animamudi <94235591+Animamudi@users.noreply.github.com>
2019-03-16 14:39:31 +0000

    New translations ja-JP.yml (Norwegian)

--- index.php
+++ index.php
@@ -278,3 +278,4 @@ $app->post('/balances', function() use ($app,&$mysqli) {
 $app->get('/blocksgaps', function() use ($app,&$mysqli) {
 
     //Create a response
+    $response = new Phalcon\Http\Response();
