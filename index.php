786bfc026e471c9120c050d2090e98245c4bf342
Animamudi <94235591+Animamudi@users.noreply.github.com>
2019-04-15 09:50:47 +0000

    Not important

--- index.php
+++ index.php
@@ -439,3 +439,4 @@ $app->post('/blocks', function() use ($app,&$mysqli) {
           $response->setStatusCode(503, "Service Unavailable");
           $response->setJsonContent(array('status' => 'ERROR', 'messages' => array($mysqli->errno.': '.$mysqli->error)));
           return $response;
+        }
