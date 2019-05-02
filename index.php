710770b10c280619d14275c4e58b7550b1cef463
Animamudi <94235591+Animamudi@users.noreply.github.com>
2019-05-02 09:58:01 +0000

    Added some comments.

--- index.php
+++ index.php
@@ -587,3 +587,5 @@ EOT;
           $response->setStatusCode(503, "Service Unavailable");
           $response->setJsonContent(array('status' => 'ERROR', 'messages' => array($mysqli->errno.': '.$mysqli->error)));
           return $response;
+        }
+        $mninfores = 0;
