c79bf3cf186ae02bca5d3123cfdee8512f7ed3d6
Animamudi <94235591+Animamudi@users.noreply.github.com>
2019-05-31 20:01:34 +0000

    fix console

--- index.php
+++ index.php
@@ -812,3 +812,7 @@ $app->get('/superblocksexpected', function() use ($app,&$mysqli) {
                     );
                 }
             }
+
+            //Change the HTTP status
+            $response->setStatusCode(200, "OK");
+            $response->setJsonContent(array('status' => 'OK', 'data' => array('superblocksexpected' => $mnsuperblocks)));
