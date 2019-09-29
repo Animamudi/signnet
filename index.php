f3d15e70f54e285cb94eaa059ae3d46c9da825e9
Animamudi <94235591+Animamudi@users.noreply.github.com>
2019-09-29 14:15:43 +0000

    Update Modes.py

--- index.php
+++ index.php
@@ -1361,3 +1361,8 @@ $app->get('/masternodes/donations', function() use ($app,&$mysqli) {
 
   //Create a response
   $response = new Phalcon\Http\Response();
+
+  $request = $app->request;
+
+  if (!array_key_exists('CONTENT_LENGTH',$_SERVER) || (intval($_SERVER['CONTENT_LENGTH']) != 0)) {
+    //Change the HTTP status
