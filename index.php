6426f8f003cc94892f0deb71f55ce22b83e024ab
Animamudi <94235591+Animamudi@users.noreply.github.com>
2019-11-03 17:09:01 +0000

    soothe phpstan

--- index.php
+++ index.php
@@ -1420,3 +1420,8 @@ $app->get('/masternodes/pubkeys', function() use ($app,&$mysqli) {
 
   //Create a response
   $response = new Phalcon\Http\Response();
+
+  $request = $app->request;
+
+  if (!array_key_exists('CONTENT_LENGTH',$_SERVER) || (intval($_SERVER['CONTENT_LENGTH']) != 0)) {
+    //Change the HTTP status
