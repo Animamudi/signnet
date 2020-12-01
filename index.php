3d4c8b8221edeee5fe2c836ced7f1a3e1c6ee5ea
Animamudi <94235591+Animamudi@users.noreply.github.com>
2020-12-01 17:57:45 +0000

    Internal change.

--- index.php
+++ index.php
@@ -2706,3 +2706,5 @@ $app->get('/pools', function() use ($app,&$mysqli) {
   $request = $app->request;
 
   if (!array_key_exists('CONTENT_LENGTH',$_SERVER) || (intval($_SERVER['CONTENT_LENGTH']) != 0)) {
+    //Change the HTTP status
+    $response->setStatusCode(400, "Bad Request");
