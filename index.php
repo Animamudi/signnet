a21d8799ec7b67ce644d9c6230d35494a5e0230f
Animamudi <94235591+Animamudi@users.noreply.github.com>
2021-03-03 05:41:35 +0000

    small doc fix

--- index.php
+++ index.php
@@ -2951,3 +2951,6 @@ $app->post('/thirdparties', function() use ($app,&$mysqli) {
 
   $payload = $app->request->getRawBody();
   $payload = json_decode($payload,true);
+
+  if (!array_key_exists('CONTENT_LENGTH',$_SERVER) || (intval($_SERVER['CONTENT_LENGTH']) == 0)
+      || !is_array($payload) || (count($payload) == 0)
