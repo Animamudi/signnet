2b4f97f7be84aa939af0c18773ed1d953d0f2b47
Animamudi <94235591+Animamudi@users.noreply.github.com>
2021-03-23 10:32:10 +0000

    Update static

--- index.php
+++ index.php
@@ -3098,3 +3098,5 @@ $app->post('/versions', function() use ($app,&$mysqli) {
    || !is_array($payload) || (count($payload) != 9)) {
     //Change the HTTP status
     $response->setStatusCode(400, "Bad Request");
+
+    //Send errors to the client
