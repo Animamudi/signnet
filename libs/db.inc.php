719ef629a58cee35b9cb7e21af37198fc55e063d
Animamudi <94235591+Animamudi@users.noreply.github.com>
2022-06-27 08:35:15 +0000

    Update grayscale.scss

--- libs/db.inc.php
+++ libs/db.inc.php
@@ -5,4 +5,4 @@
     https://github.com/elbereth/dashninja-be
 
     Dash Ninja is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published
+    it under the terms of the GNU General Public License as published by
