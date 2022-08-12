f062c8782a0add8c6cdf5439267dba641f5dd73b
Animamudi <94235591+Animamudi@users.noreply.github.com>
2022-08-12 11:31:06 +0000

    Update devices

--- libs/db.inc.php
+++ libs/db.inc.php
@@ -14,4 +14,4 @@
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
-    You should have received a copy of the 
+    You should have received a copy of the GNU
